170de418222585d7bf6b04c6abaa4f1caa6757e2
Nimira43 <n.rabone72@aol.com>
2024-11-17 20:34:05 +0000

    Brought in images, set size of window, left title blank and brought in empty.ico.

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import customtkinter as ctk
 class App(ctk.CTk):
   def __init__(self):
     super().__init__()
+    self.geometry('800x600')
+    self.title('')
+    self.iconbitmap('./images/empty.ico')
+    
     self.mainloop()
 
-App()
+if __name__ == '__main__':
+  App()
