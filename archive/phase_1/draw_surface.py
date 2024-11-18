adbf312ea64ccad3d4fa56061781d7c5aecaf86a
Nimira43 <n.rabone72@aol.com>
2024-11-18 20:56:10 +0000

    Created archive of code so far.

---
+++ archive/phase_1/draw_surface.py
@@ -0,0 +1,34 @@
+from tkinter import Canvas
+from setting import *
+
+class DrawSurface(Canvas):
+  def __init__(self, parent, colour_string, brush_float):
+    super().__init__(master = parent,background = CANVAS_BG, bd = 0, highlightthickness = 0, relief='ridge')
+    self.pack(expand = True, fill = 'both')
+
+    self.colour_string = colour_string
+    self.brush_float = brush_float
+    self.allow_draw = False
+
+    self.old_x = None
+    self.old_y = None
+
+    self.bind('<Motion>', self.draw)
+    self.bind('<Button>', self.activate_draw)
+    self.bind('<ButtonRelease>', self.deactivate_draw)
+
+  def draw(self, event):
+    if self.allow_draw:
+      if self.old_x and self.old_y:
+        self.create_line((self.old_x, self.old_y), (event.x, event.y), fill = 'black')
+      self.old_x = event.x
+      self.old_y = event.y
+
+
+  def activate_draw(self, event):
+    self.allow_draw = True
+
+  def deactivate_draw(self, event):
+    self.allow_draw = False
+    self.old_x = None
+    self.old_y = None
---
+++ archive/phase_1/setting.py
@@ -0,0 +1,20 @@
+COLOUR_RANGE = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+COLOURS = (
+  ('000', '999', 'BBB', 'FFF'),
+  ('400', '900', 'B00', 'F00'),
+  ('040', '090', '0B0', '0F0'),
+  ('004', '009', '00B', '00F'),
+  ('F0F', 'FF0', '0FF', 'F66'),
+  ('94B', 'F90', '088', 'EAA'),
+) 
+COLOUR_COLS = len(COLOURS[0])
+COLOUR_ROWS = len(COLOURS)
+
+CANVAS_BG = 'white'
+BUTTON_COLOUR = 'gray'
+BUTTON_HOVER_COLOUR = 'gray'
+BUTTON_ACTIVE_COLOUR = '#dbdbdb'
+BRUSH_PREVIEW_BG = '#ebebeb'
+SLIDER_RED = '#E00'
+SLIDER_GREEN = '#0E0'
+SLIDER_BLUE = '#00E'
