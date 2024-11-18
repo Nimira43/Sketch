ca66a2e1f56ada9ed3a5cac5c123642f15d9617b
Nimira43 <n.rabone72@aol.com>
2024-11-18 20:24:28 +0000

    Completed setup of the colour and size of brush

--- draw_surface.py
+++ draw_surface.py
@@ -5,3 +5,5 @@ class DrawSurface(Canvas):
   def __init__(self, parent, colour_string, brush_float):
     super().__init__(master = parent,background = CANVAS_BG, bd = 0, highlightthickness = 0, relief='ridge')
     self.pack(expand = True, fill = 'both')
+    self.colour_string = colour_string
+    self.brush_float = brush_float
