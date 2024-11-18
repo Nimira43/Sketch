889825e9ff7bfbf75b4a6620fccbae1e6fd2609c
Nimira43 <n.rabone72@aol.com>
2024-11-18 20:28:18 +0000

    Starting to set up input methods to handle brush input.

--- draw_surface.py
+++ draw_surface.py
@@ -5,5 +5,10 @@ class DrawSurface(Canvas):
   def __init__(self, parent, colour_string, brush_float):
     super().__init__(master = parent,background = CANVAS_BG, bd = 0, highlightthickness = 0, relief='ridge')
     self.pack(expand = True, fill = 'both')
+
     self.colour_string = colour_string
     self.brush_float = brush_float
+
+    self.bind('<Motion>', self.draw)
+    self.bind('<Button>', self.activate_draw)
+    self.bind('<ButtonRelease>', self.deactivate_draw)
