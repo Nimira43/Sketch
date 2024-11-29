17b92a95c464de263711905bff9cb210ceb29b8a
Nimira43 <n.rabone72@aol.com>
2024-11-29 10:48:05 +0000

    Program working fine

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from draw_surface import DrawSurface
 class App(ctk.CTk):
   def __init__(self):
     super().__init__()
+    
     self.geometry('800x600')
     self.title('')
     self.iconbitmap('./images/empty.ico')
